6d58361e46623ab8fce13dd5e86474e775aa3cc6
Deepaksai1724 <gdeepaksai@gmail.com>
2026-01-18 18:18:17 +0530

    Implement error saving functionality
    Add save_error function to handle errors

--- assignment1/Q3.py
+++ assignment1/Q3.py
@@ -1,1 +1,10 @@
 #Question3
+def save_error(error, errors=None):
+    if errors is None:
+        errors = []
+    errors.append(error)
+    return errors
+
+print(save_error("Invalid Input"))
+print(save_error("unrecnognised string"))
+print(save_error("E3"))
