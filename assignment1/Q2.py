8264f333f47699ace0c89841d9e69e2fbf66ac4b
Deepaksai1724 <gdeepaksai@gmail.com>
2026-01-18 18:16:46 +0530

    Add function to manage orders with order IDs

--- assignment1/Q2.py
+++ assignment1/Q2.py
@@ -1,1 +1,9 @@
 #Question2
+def add_order(order_id, orders=None):
+    if orders is None:
+        orders = []
+    orders.append(order_id)
+    return orders
+
+print(add_order(101))
+print(add_order(103))
