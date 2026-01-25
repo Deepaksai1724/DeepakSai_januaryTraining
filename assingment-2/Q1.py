faf87f61c025f0cbf32c0278c87fad85a6686e8c
Deepaksai1724 <gdeepaksai@gmail.com>
2026-01-25 22:51:53 +0530

    Update Q1.py

--- assingment-2/Q1.py
+++ assingment-2/Q1.py
@@ -1,6 +1,6 @@
 #QUESTION-1
 import pandas as pd
-df = pd.read_csv("indian_engineering_student_placement.csv")
+df = pd.read_csv(r"C:\Users\graju\OneDrive\Documents\AENEXZ\indian_engineering_student_placement.csv")
 # Display basic info
 print("Shape of dataset:", df.shape)
 print("\nColumn names:\n", df.columns)
