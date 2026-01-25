dce8924d4d66d8f73a90b5cf2f9e97757b53cf27
Deepaksai1724 <gdeepaksai@gmail.com>
2026-01-25 22:50:25 +0530

    Update Q1.py

--- assingment-2/Q1.py
+++ assingment-2/Q1.py
@@ -1,1 +1,8 @@
 #QUESTION-1
+import pandas as pd
+df = pd.read_csv("indian_engineering_student_placement.csv")
+# Display basic info
+print("Shape of dataset:", df.shape)
+print("\nColumn names:\n", df.columns)
+print(df.info())
+print(df.head())
